2087007a1cf1729c51d514c4257612c8f8ea6163
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-03-30 13:57:46 -0700

    add form input areas to select a sauce and cheeses and integrate their selections into the js logic. add an area for the calculated price to display on index

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-function Pizza(toppings, size) {
-  this.toppings = toppings;
+function Pizza(size, sauce, toppings, cheeses) {
   this.size = size;
+  this.sauce = sauce;
+  this.toppings = toppings;
+  this.cheeses = cheeses;
 }
 
 Pizza.prototype.findPrice = function() {
-  var total = (this.toppings * 0.8) + (this.size / 1.5);
-  return total;
+  var price = (this.toppings * 0.8) + (this.size / 2) + (this.sauce) + (this.cheeses * 2 );
+  return price;
 }
 
 // function Order() {
@@ -18,12 +20,14 @@ Pizza.prototype.findPrice = function() {
 
 $(function() {
   $('form#add-pizza').submit(function(event) {
-    var selectedToppings = parseInt($('input:checkbox[name=toppings]:checked').length);
-    var pizzaSize = parseInt($('select#select-size').val());
-    var pizza = new Pizza(selectedToppings, pizzaSize);
-    var pizzaPrice = pizza.findPrice();
-    
+    var selectedSize = parseInt($('select#select-size').val());
+    var selectedSauce = parseInt($('select#select-sauce').val());
+    var checkedToppings = parseInt($('input:checkbox[name=toppings]:checked').length);
+    var checkedCheeses = parseInt($('input:checkbox[name=cheeses]:checked').length);
+    var newPizza = new Pizza(selectedSize, selectedSauce, checkedToppings, checkedCheeses);
+    var newPizzaPrice = newPizza.findPrice();
 
+    $('#output').text('Cost: $' + newPizzaPrice.toFixed(2));
     event.preventDefault();
   });
 });
