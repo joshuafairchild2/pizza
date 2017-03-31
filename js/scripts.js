0c3542ebf482c5a423bda27fa191b636b6909ccd
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-03-31 14:46:16 -0700

    separated some js logic, defined the findTotal method and integrated it into the ui logic.

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,15 @@ function Order() {
   this.customer = [];
 }
 
-// Order.protoype.findTotal = function() {
-//
-// }
+Order.prototype.findTotal = function() {
+  var pizzasTotal = 0;
+  for (var i = 0; i < this.pizzas.length; i++) {
+    pizzasTotal += this.pizzas[i].findPrice();
+  }
+  var extrasTotal = this.extras * 3;
+  var orderTotal = pizzasTotal + this.drinks + extrasTotal;
+  return orderTotal;
+}
 
 
 $(function() {
@@ -43,64 +49,65 @@ $(function() {
     $(this).trigger('reset');
     $('#item-added').modal('show');
     $('#item').text('Pizza');
+    console.log(newPizza);
+    event.preventDefault();
+  });
 
-    $('#item-modal-btn').click(function() {
-      $('#item-added').modal('hide');
-    });
+  $('#item-modal-btn').click(function() {
+    $('#item-added').modal('hide');
+  });
 
-    $('#continue-btn1').click(function() {
-      $('#add-pizza').hide();
-      $('#drinks-extras').show();
-    });
+  $('#continue-btn1').click(function() {
+    $('#add-pizza').hide();
+    $('#drinks-extras').show();
+  });
 
-    $('form#add-drinks').submit(function(event) {
-      var selectedDrink = $('input:radio[name=drinks]:checked');
-      if (selectedDrink.val()) {
-        order.drinks += parseInt(selectedDrink.length);
-        $('.drinks').text(order.drinks);
-        $(this).trigger('reset');
-        $('#item-added').modal('show');
-        $('#item').text('1 ' + selectedDrink.val())
-      }
-      event.preventDefault();
-    });
+  $('form#add-drinks').submit(function(event) {
+    var selectedDrink = $('input:radio[name=drinks]:checked');
+    if (selectedDrink.val()) {
+      order.drinks += parseInt(selectedDrink.length);
+      $('.drinks').text(order.drinks);
+      $(this).trigger('reset');
+      $('#item-added').modal('show');
+      $('#item').text('1 ' + selectedDrink.val())
+    }
+    event.preventDefault();
+  });
 
-    $('form#add-extras').submit(function(event) {
-      var selectedExtra = $('input:radio[name=extras]:checked');
-      if (selectedExtra.val()) {
-        order.extras += parseInt(selectedExtra.length);
-        $('.extras').text(order.extras);
-        $(this).trigger('reset');
-        $('#item-added').modal('show');
-        $('#item').text(selectedExtra.val());
-      }
-      event.preventDefault();
-    });
+  $('form#add-extras').submit(function(event) {
+    var selectedExtra = $('input:radio[name=extras]:checked');
+    if (selectedExtra.val()) {
+      order.extras += parseInt(selectedExtra.length);
+      $('.extras').text(order.extras);
+      $(this).trigger('reset');
+      $('#item-added').modal('show');
+      $('#item').text(selectedExtra.val());
+    }
+    event.preventDefault();
+  });
 
-    $('#continue-btn2').click(function() {
-      $('#drinks-extras').hide();
-      $('#customer-info').show();
-    });
+  $('#continue-btn2').click(function() {
+    $('#drinks-extras').hide();
+    $('#customer-info').show();
+  });
 
-    $('form#customer-info').submit(function(event) {
-      var inputName = $('input#customer-name').val();
-      var inputNumber = $('input#customer-number').val();
-      var inputAddress = $('input#customer-address').val();
-      var inputPaymentMethod = $('select#cash-or-card').val();
-      var customer = new Customer(inputName, inputNumber, inputAddress, inputPaymentMethod);
-      order.customer.push(customer);
-
-      $('#order-helper').addClass('invisible');
-      $('#customer-info').hide();
-      $('#confirmation').show();
-      $('#name').text(customer.fullName);
-      $('#number').text(customer.number);
-      $('#address').text(customer.address);
-      $('#payment-method').text(customer.paymentMethod);
-      event.preventDefault();
-    });
-
-
+  $('form#customer-info').submit(function(event) {
+    var inputName = $('input#customer-name').val();
+    var inputNumber = $('input#customer-number').val();
+    var inputAddress = $('input#customer-address').val();
+    var inputPaymentMethod = $('select#cash-or-card').val();
+    var customer = new Customer(inputName, inputNumber, inputAddress, inputPaymentMethod);
+    var newTotal = order.findTotal();
+    order.customer.push(customer);
+    console.log(newTotal);
+    $('#order-helper').addClass('invisible');
+    $('#customer-info').hide();
+    $('#output-total').text(newTotal)
+    $('#confirmation').show();
+    $('#name').text(customer.fullName);
+    $('#number').text(customer.number);
+    $('#address').text(customer.address);
+    $('#payment-method').text(customer.paymentMethod);
     event.preventDefault();
   });
 });
