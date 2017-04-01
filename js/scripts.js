d269377a85cc475becb260dbae2fac43c05cf0ab
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-04-01 05:33:55 -0700

    add another modal to index that will be used when invalid forms are entered, add scripting to the modal in js ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -42,7 +42,7 @@ $(function() {
     var selectedSauce = parseInt($('select#select-sauce').val());
 
     if (isNaN(selectedSize) || isNaN(selectedSauce)) {
-      alert('sorry bub');
+      $('#invalid-form').modal('show');
     } else {
       var checkedToppings = parseInt($('input:checkbox[name=toppings]:checked').length);
       var checkedCheeses = parseInt($('input:checkbox[name=cheeses]:checked').length);
@@ -63,8 +63,8 @@ $(function() {
   });
 
 
-  $('#item-modal-btn').click(function() {
-    $('#item-added').modal('hide');
+  $('.modal-btn').click(function() {
+    $('.modal').modal('hide');
   });
 
 
@@ -114,7 +114,7 @@ $(function() {
     var inputPaymentMethod = $('select#cash-or-card').val();
 
     if (!inputName || !inputNumber || !inputAddress || !inputPaymentMethod) {
-      alert('sorry bub'); //make this form validation better
+      $('#invalid-form').modal('show');
     } else {
       var customer = new Customer(inputName, inputNumber, inputAddress, inputPaymentMethod);
       var newTotal = order.findTotal();
